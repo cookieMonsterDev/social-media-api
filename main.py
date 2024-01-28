2d991ad32cf4eefc0d379cbae73606fb59d702f7
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-28 11:17:30 +0200

    feat: try crud operations

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -11,13 +12,30 @@ class CreatePostDto(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+my_posts = []
+
+@app.get("/posts")
+def get_posts():
+    return my_posts
+
+@app.get('/posts/{id}')
+def get_post(id: int):
+    data = my_posts[id]
+    return jsonable_encoder(data)
 
 @app.post('/posts')
 def create_post(createPostDto: CreatePostDto):
-    try: 
-        return { "test": createPostDto.title }
-    except ValidationError as error:
-        return { "error": 'test error' }
+    data = createPostDto.model_dump()
+    my_posts.append(data)
+    return jsonable_encoder(data)
+
+@app.patch('/posts/{id}')
+def update_post(id: int, createPostDto: CreatePostDto):
+    data = createPostDto.model_dump()
+    my_posts[id] = data
+    return jsonable_encoder(data)
+
+@app.delete('posts/{id}')
+def delete_post(id: int):
+    del my_posts[id]
+    return
