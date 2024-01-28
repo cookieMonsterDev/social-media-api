0eca21a72c45d1898f6106058d241ba8477f10b8
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-28 14:38:36 +0200

    feat: try error handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
@@ -7,21 +7,52 @@ from fastapi.encoders import jsonable_encoder
 app = FastAPI()
 
 class CreatePostDto(BaseModel):
+    id: int
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
 
-my_posts = []
+my_posts = [
+     {
+        "id": 1,
+        "title": "test 12",
+        "content": "test content",
+        "published": True,
+        "rating": None
+    },
+    {
+        "id": 2,
+        "title": "test 12",
+        "content": "test content",
+        "published": True,
+        "rating": None
+    },
+    {
+        "id": 3,
+        "title": "test 12",
+        "content": "test content",
+        "published": True,
+        "rating": None
+    }
+]
 
+def findPostById(id: int):
+    for item in my_posts:
+        if item['id'] == id:
+            return item
+    return None    
+    
 @app.get("/posts")
 def get_posts():
     return my_posts
 
 @app.get('/posts/{id}')
-def get_post(id: int):
-    data = my_posts[id]
-    return jsonable_encoder(data)
+def get_post(id: int, res: Response):
+    data = findPostById(id) 
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=None)
+    return { "data": data }
 
 @app.post('/posts')
 def create_post(createPostDto: CreatePostDto):
@@ -35,7 +66,7 @@ def update_post(id: int, createPostDto: CreatePostDto):
     my_posts[id] = data
     return jsonable_encoder(data)
 
-@app.delete('posts/{id}')
+@app.delete('/posts/{id}')
 def delete_post(id: int):
     del my_posts[id]
     return
