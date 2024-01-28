dd181aaaed204f78f226634230c3cf5254fde43f
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-28 10:52:55 +0200

    feat: try body validation with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel, ValidationError
 
 app = FastAPI()
+
+class CreatePostDto(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post('/posts')
+def create_post(createPostDto: CreatePostDto):
+    try: 
+        return { "test": createPostDto.title }
+    except ValidationError as error:
+        return { "error": 'test error' }
